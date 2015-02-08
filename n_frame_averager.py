1bb69fc897c4d3f80fe30ad5bdbd4c405dcf6a9d
Steve Geluso <stevegeluso@gmail.com>
2015-02-08 05:36:42 -0800

    init commit

---
+++ n_frame_averager.py
@@ -0,0 +1,14 @@
+WIDTH = 2
+MAXX = 1560
+
+IN_DIR = "original_frames"
+OUT_DIR = "ave2_frames/"
+
+for i in range(1, MAXX):
+  frame_range = range(max(1, i - WIDTH), min(MAXX, i + WIDTH))
+  files = ""
+  for frame in frame_range:
+    files += "%s/%04d.png " % (IN_DIR, frame)
+  command = "convert %s -evaluate-sequence mean %s/%04d.png" % (files, OUT_DIR, i)
+  print "echo '(%d/%d)'" % (i, MAXX)
+  print command
